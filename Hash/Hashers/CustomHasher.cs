55792da4b152e03c9f9f92ea3b8d3fe58543b8e9
Julius Truksinas <julius.truksinasinfo@gmail.com>
2025-09-23 20:47:24 +0300

    creat starting constants for the custom hasher

--- Hash/Hashers/CustomHasher.cs
+++ Hash/Hashers/CustomHasher.cs
@@ -5,6 +5,11 @@ namespace Hash.Hashers;
 
 public class CustomHasher : IHasher
 {
+    private const ulong START_CONST1 = 0x243F6A8885A308D3;
+    private const ulong START_CONST2 = 0x13198A2E03707344;
+    private const ulong START_CONST3 = 0xA4093822299F31D0;
+    private const ulong START_CONST4 = 0x082EFA98EC4E6C89;
+
     public string Hash(string input)
     {
         return "hashed value";
