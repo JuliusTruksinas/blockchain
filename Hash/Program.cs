38487868634ee9bf3dff32750869d1a84f71ddc5
Julius Truksinas <julius.truksinasinfo@gmail.com>
2025-09-30 18:23:58 +0300

    minor fix

--- Hash/Program.cs
+++ Hash/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Hash.Enums;
 using Hash.Hashers;
-using Hash.Helpers;
 using Hash.InputProviders;
 using Hash.Interfaces;
+using Hash.Models;
 
 namespace Blockchain;
 
@@ -10,22 +10,19 @@ public class Program
 {
     static int Main(string[] args)
     {
-        FileGenerator.GenerateFileWithOneCharacter('a');
+        IHashInputProvider inputProvider = GetInputProvider(args);
+        if (!inputProvider.TryGetInput(out HashInput? input, out string? errorMessage))
+        {
+            Console.WriteLine(errorMessage);
+            return 1;
+        }
+
+        IHasher hasher = GetHasher(input!.HashAlgorithm);
+        string hashedContent = hasher.Hash(input.Content);
+
+        Console.WriteLine($"Hashed content: {hashedContent}");
+
         return 0;
-
-        //IHashInputProvider inputProvider = GetInputProvider(args);
-        //if (!inputProvider.TryGetInput(out HashInput? input, out string? errorMessage))
-        //{
-        //    Console.WriteLine(errorMessage);
-        //    return 1;
-        //}
-
-        //IHasher hasher = GetHasher(input!.HashAlgorithm);
-        //string hashedContent = hasher.Hash(input.Content);
-
-        //Console.WriteLine($"Hashed content: {hashedContent}");
-
-        //return 0;
     }
 
     private static IHashInputProvider GetInputProvider(string[] args)
